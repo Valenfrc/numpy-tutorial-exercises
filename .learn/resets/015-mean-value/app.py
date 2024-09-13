79a10faa0103c1116193d5c4cf4ae811520836cd
Valenfrc <val21.frc@gmail.com>
2024-09-13 22:25:54 +0000

    015

---
+++ .learn/resets/015-mean-value/app.py
@@ -0,0 +1,29 @@
+import numpy as np
+print(np.__version__)
+print(np.zeros(10))
+zeros=np.zeros(10) 
+mem_size=zeros.itemsize * zeros.size
+print(mem_size)
+print(np.info(np.add))
+arr = np.zeros(10)
+arr[4]=1
+print(arr)
+arr=np.arange(10,50)
+print(arr)
+vector=np.arange(10)
+vector_invertido=vector[::-1]
+print(vector_invertido)
+import numpy as np
+arr=np.arange(0,9)
+matriz= np.reshape(arr,(3,3))
+print(matriz)
+arr = np.array([1,2,0,0,4,0])
+print(np.nonzero(arr))
+matriz=np.eye(3,3)
+print(matriz)
+arr=np.random.random(3)
+print(arr)
+arr=np.random.random(10)
+print(arr.max())
+arr=np.random.random(10)
+print(arr.mean(5))
