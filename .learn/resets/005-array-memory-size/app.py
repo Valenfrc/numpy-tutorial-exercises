0e021a0db56ef1926900ea285ca19dd373373009
Valenfrc <val21.frc@gmail.com>
2024-09-13 00:26:26 +0000

    007

---
+++ .learn/resets/005-array-memory-size/app.py
@@ -0,0 +1,6 @@
+import numpy as np
+print(np.__version__)
+print(np.zeros(10))
+mem_size=zero.itemsize * zeros.size
+print(mem_size)
+
--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 import numpy as np
+print(np.__version__)
+print(np.zeros(10))
+zeros=np.zeros(10) 
+mem_size=zeros.itemsize * zeros.size
+print(mem_size)
+print(np.info(np.add))
+arr = np.zeros(10)
+arr(5)=1
+
